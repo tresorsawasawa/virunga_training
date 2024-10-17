9ed73ff3297755ec750d8cb5a55c19bf552e3136
tresorsawasawa <tresorsawasawa@gmail.com>
2024-10-17 08:42:03 +0200

    Fix a bug

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Real Estate",
+    "summary": "Test module",
+    "version": "17.0.0.0",
+    "license": "OEEL-1",
+    "author": "Tresor Sawasawa",
+    "category": "Test",
+    "depends": ["base"],
+    "data": [],
+    "application": True,
+}
