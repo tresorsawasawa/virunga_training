f9cc5526711afd08a618177902638839ece206d9
tresorsawasawa <tresorsawasawa@gmail.com>
2024-10-21 12:59:29 +0200

    Add the real estate property type table

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -39,4 +39,7 @@ class EstateProperty(models.Model):
         copy=False,
         required=True
         )
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string= "Buyer", copy=False)
+    salesperson_id = fields.Many2one("res.users", string= "Salesperson", default=lambda self: self.env.user)
 
