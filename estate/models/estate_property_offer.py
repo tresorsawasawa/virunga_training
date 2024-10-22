6cc8646e75866b35a78731e90fd58554c989370e
tresorsawasawa <tresorsawasawa@gmail.com>
2024-10-22 12:05:37 +0200

    Compute a validity date for offers

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,14 +1,36 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta, datetime
 
 class EstatePropertyOffer(models.Model):
-    _name = 'estate.property.offer'
-    _description = 'Property Offer'
+    _name = "estate.property.offer"
+    _description = "Property Offer"
 
     price = fields.Float(string="Price")
-    status = fields.Selection([
-        ('accepted', 'Accepted'),
-        ('refused', 'Refused')
-    ], string='Status', copy=False)
-    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
-    property_id = fields.Many2one('estate.property', string='Property', required=True)
-    property_type_id = fields.Many2one(related='property_id.property_type_id', store=True, string='Property Type')
+    status = fields.Selection(
+        [("accepted", "Accepted"), ("refused", "Refused")], string="Status", copy=False
+    )
+    partner_id = fields.Many2one("res.partner", string="Partner", required=True)
+    property_id = fields.Many2one("estate.property", string="Property", required=True)
+    property_type_id = fields.Many2one(
+        related="property_id.property_type_id", store=True, string="Property Type"
+    )
+    validity = fields.Integer(default=7, string="Validity (Days)")
+    date_deadline = fields.Date(
+        string="Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+        store=True,
+    )
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for offer in self:
+            create_date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.date_deadline = create_date + timedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            if offer.date_deadline and offer.create_date:
+                offer.validity = (offer.date_deadline - offer.create_date.date()).days
+            elif offer.date_deadline:
+                offer.validity = (offer.date_deadline - fields.Date.today()).days
