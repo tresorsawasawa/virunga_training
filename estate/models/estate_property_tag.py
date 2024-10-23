5fc828237b82be6e4fb64a3ad0b5cf88c69b507e
tresorsawasawa <tresorsawasawa@gmail.com>
2024-10-23 10:11:46 +0200

    Correct some typo errors

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 class EstatePropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Property Tag'
-    _sql_constrains = [('unique_name', 'UNIQUE(name)', 'Tag name has to be unique')]
+    _sql_constraints = [('unique_name', 'UNIQUE(name)', 'Tag name has to be unique')]
 
     name = fields.Char(string='Name', required=True)
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name="estate.property.type"
     _description="Real Estate Property Type"
-    _sql_constrains = [("unique_name", "UNIQUE(name)", "Type name has to be unique")]
+    _sql_constraints = [("unique_name", "UNIQUE(name)", "Type name has to be unique")]
 
     name = fields.Char(string="Name", required=True)
