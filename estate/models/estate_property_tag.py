eb6c538c7e877ec2ffc6fd838f06d0ff40a2ac4b
tresorsawasawa <tresorsawasawa@gmail.com>
2024-10-23 14:20:02 +0200

    Add the Model and Manual orderings

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -5,6 +5,7 @@ class EstatePropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Property Tag'
     _sql_constraints = [('unique_name', 'UNIQUE(name)', 'Tag name has to be unique')]
+    _order = "name"
 
     name = fields.Char(string='Name', required=True)
     color = fields.Integer()
