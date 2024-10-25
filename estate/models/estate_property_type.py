5ad76032d0492cf9634a4c7c4b6dd53b68362dbd
tresorsawasawa <tresorsawasawa@gmail.com>
2024-10-25 09:49:41 +0200

    Add the more button on the estate.property.type

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstatePropertyType(models.Model):
@@ -10,3 +10,24 @@ class EstatePropertyType(models.Model):
 
     sequence = fields.Integer(default=1)
     name = fields.Char(string="Name", required=True)
+    offer_ids = fields.One2many('estate.property.offer', 'property_type_id', string='Offers')
+    offer_count = fields.Integer(string="Property Count", compute="_compute_offer_count")
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
+
+    
+    def action_open_property_ids(self):
+        return  {
+            "name" : "Offers",
+            "type" : "ir.actions.act_window",
+            "view_mode" : "tree,form",
+            "res_model" : "estate.property.offer",
+            "target" : "current",
+            "domain" : [("property_type_id", "=", self.id)],
+            "context" : {
+                "default_property_type_id" : self.id
+            }
+        }
